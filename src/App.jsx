26f03e3c616a3b5398adccbb06a912580487f75b
Iulian Roman <iulian.roman@cognizant.com>
2026-02-23 16:40:12 +0200

    created landing page, added footer to app, styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import "./App.css";
 import { Outlet } from "react-router";
-
+import { NavLink } from "react-router";
 function App() {
-  return <Outlet />;
+  return (
+    <div className="app">
+      <Outlet />
+      <footer className="footer">
+        <p>&copy; All rights reserved.</p>
+        <NavLink to="/contact" className="contact-link">
+          Contact us
+        </NavLink>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
